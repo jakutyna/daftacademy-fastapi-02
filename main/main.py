2f08d6b4d33d9d0924061df49ad88fe4091843b6
Jacek <jakutyna@gmail.com>
2021-05-01 14:05:16 +0200

    Ex1 v2

--- main/main.py
+++ main/main.py
@@ -1,9 +1,11 @@
 from datetime import date
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
 
 # Ex1
@@ -19,3 +21,10 @@ def hello_view():
             </body>
         </html>
         """
+
+
+# Ex1 with templates
+@app.get('/hello2', response_class=HTMLResponse)
+def hello2_view(request: Request):
+    current_date = str(date.today())
+    return templates.TemplateResponse("hello2.html", {"request": request, "current_date": current_date})
