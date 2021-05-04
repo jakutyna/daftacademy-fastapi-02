052164e125530741c0fb248ca42fd91daa6fcf58
Jacek <jakutyna@gmail.com>
2021-05-04 21:51:02 +0200

    Add ex5

--- app/dependencies.py
+++ app/dependencies.py
@@ -13,11 +13,30 @@ security = HTTPBasic()
 
 
 def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    """
+    Gets and verifies credentials from 'authentication' header
+    """
     correct_username = secrets.compare_digest(credentials.username, '4dm1n')
     correct_password = secrets.compare_digest(credentials.password, 'NotSoSecurePa$$')
     if not (correct_username and correct_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
     return credentials.username, credentials.password
+
+
+def logout_json_html(query_params):
+    """ Checks if there is 'format' in query parameters and returns response in correct format. """
+    if 'format' in query_params:
+        response_format = query_params['format']
+        if response_format == 'json':
+            return JSONResponse(content={'message': 'Logged out!'})
+        elif response_format == 'html':
+            return HTMLResponse(content='<h1>Logged out!</h1>')
+    return PlainTextResponse(content='Logged out!')
+
+
+def verify_token(tokens_cache, token):
+    if token not in tokens_cache:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
 
 
 def random_token():
@@ -33,13 +52,3 @@ def response_json_html(query_params):
         elif response_format == 'html':
             return HTMLResponse(content='<h1>Welcome!</h1>')
     return PlainTextResponse(content='Welcome!')
-
-
-def logout_json_html(query_params):
-    if 'format' in query_params:
-        response_format = query_params['format']
-        if response_format == 'json':
-            return JSONResponse(content={'message': 'Logged out!'})
-        elif response_format == 'html':
-            return HTMLResponse(content='<h1>Logged out!</h1>')
-    return PlainTextResponse(content='Logged out!')
--- app/routers/login.py
+++ app/routers/login.py
@@ -1,90 +1,75 @@
 from typing import Optional
 
 from fastapi import APIRouter, Cookie, Depends, HTTPException, status, Query, Request, Response
-from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse, RedirectResponse
+from fastapi.responses import RedirectResponse
 
-from ..dependencies import authenticate, random_token, response_json_html, logout_json_html
+from ..dependencies import authenticate, logout_json_html, random_token, response_json_html, verify_token
 
 router = APIRouter(
     tags=['login'],
 )
 
 # Tokens cached on server
-router.session_token = []
-router.json_token = []
+router.session_tokens = []
+router.json_tokens = []
 
 
 # Ex2
 @router.post('/login_session', status_code=status.HTTP_201_CREATED, dependencies=[Depends(authenticate)])
 def login_session_view(response: Response):
+    """ Verifies credentials and generates random session token kept in cookie. """
     session_token = random_token()
-    if len(router.session_token) == 0:
-        router.session_token.append(session_token)
-    else:
-        router.session_token[0] = session_token
+    router.session_tokens.append(session_token)
+    if len(router.session_tokens) > 3:
+        del router.session_tokens[0]
     response.set_cookie(key='session_token', value=session_token)
     return {'message': 'You are logged'}
 
 
 @router.post('/login_token', status_code=status.HTTP_201_CREATED, dependencies=[Depends(authenticate)])
 def login_session_view():
+    """ Verifies credentials and generates random session token kept in json file. """
     json_token = random_token()
-    if len(router.json_token) == 0:
-        router.json_token.append(json_token)
-    else:
-        router.json_token[0] = json_token
+    router.json_tokens.append(json_token)
+    if len(router.json_tokens) > 3:
+        del router.json_tokens[0]
     return {'message': 'You are logged', "token": json_token}
 
 
 # Ex3
 @router.get('/welcome_session')
 def welcome_session_view(request: Request, session_token: Optional[str] = Cookie(None)):
-    try:
-        if session_token != router.session_token[0]:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
-    except IndexError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
+    verify_token(router.session_tokens, session_token)
     return response_json_html(request.query_params)
 
 
 @router.get('/welcome_token')
 def welcome_token_view(request: Request, token: Optional[str] = Query(None)):
-    try:
-        if token != router.json_token[0]:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
-    except IndexError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
+    verify_token(router.json_tokens, token)
     return response_json_html(request.query_params)
 
 
 # Ex4
 @router.delete('/logout_session')
 def logout_session_view(request: Request, session_token: Optional[str] = Cookie(None)):
-    try:
-        if session_token != router.session_token[0]:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
-    except IndexError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
-    del router.session_token[0]
+    verify_token(router.session_tokens, session_token)
+    router.session_tokens.remove(session_token)
     url = f'/logged_out?format={request.query_params["format"]}' if 'format' in request.query_params else '/logged_out'
     return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
 
 
 @router.delete('/logout_token')
 def logout_token_view(request: Request, token: Optional[str] = Query(None)):
-    try:
-        if token != router.json_token[0]:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
-    except IndexError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not Authorized')
-    del router.json_token[0]
+    verify_token(router.json_tokens, token)
+    router.json_tokens.remove(token)
     url = f'/logged_out?format={request.query_params["format"]}' if 'format' in request.query_params else '/logged_out'
     return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
 
 
 @router.get('/logged_out')
 def logout_session_view(request: Request):
-    return logout_json_html(request.query_params)
+    return {'session_cache': router.session_tokens, 'json_cache': router.json_tokens}
+    # return logout_json_html(request.query_params)
 
 
 # View for testing authorization and 'Depends' method
